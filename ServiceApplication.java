fa9b43859a540aedd4e7ef7ab515a4f18350c1c3
divyasri09 <38257961+divyasri09@users.noreply.github.com>
2018-04-10 12:38:05 -0400

    Update ServiceApplication.java

--- ServiceApplication.java
+++ ServiceApplication.java
@@ -8,22 +8,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 /**
  * 
  */
-
-/**
- * This is the main class of the checkout service spring boot application which starts that service
- * 
- * @author VXJ8774
- *
- */
 @SpringBootApplication
 public class CheckoutServiceApplication {
     
-    public CheckoutServiceApplication() {       //NOSONAR: Avoiding squid:S1118 as the constructor is required to be public for SpringAppilcation.run()
+    public ServiceApplication() {       //NOSONAR: Avoiding squid:S1118 as the constructor is required to be public for SpringAppilcation.run()
         super();
     }
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(CheckoutServiceApplication.class, args);     //NOSONAR: Cannot change this to ConfigurableApplicationContext just to close it
+        ApplicationContext ctx = SpringApplication.run(ServiceApplication.class, args);     //NOSONAR: Cannot change this to ConfigurableApplicationContext just to close it
 
         //this is essential as it allows our application to address 404 errors with a common payload
         DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
